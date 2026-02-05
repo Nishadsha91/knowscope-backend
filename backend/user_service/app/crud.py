378609258e38d0a99fcbc26b226cb26fea51ea7d
knowscope80 <EMAIL_USED_IN_KNOWSCOPE80>
2026-02-05 10:20:42 +0530

    comm

--- backend/user_service/app/crud.py
+++ backend/user_service/app/crud.py
@@ -6,6 +6,9 @@ def get_user_by_email(db: Session, email: str):
     return db.query(User).filter(User.email == email).first()
 
 
+
+
+
 def create_oauth_user(db: Session, email: str, full_name: str):
     user = User(email=email,full_name=full_name,password=None )
     db.add(user)
