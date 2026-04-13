189f55e2cccfefc35c1212d8b94cf5f4c45fe78c
Nishadsha91 <muthumattilnishad91@gmail.com>
2026-04-13 10:58:44 +0530

    local updates

--- backend/user_service/app/routes/auth.py
+++ backend/user_service/app/routes/auth.py
@@ -1,5 +1,5 @@
 import os
-from datetime import datetime, timedelta
+from datetime import datetime
 
 from dotenv import load_dotenv
 from fastapi import APIRouter, Body, Depends, Header, HTTPException
